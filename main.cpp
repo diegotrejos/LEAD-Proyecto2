11562f698fedd2aaaf17a7b20e4b7fc9020e61c1
LuisValverde1296 <luis.valverdecruz@ucr.ac.cr>
2019-03-31 23:50:01 -0600

    Add files via upload
    Main que abre directorio pasado por argumentos y clasifica los archivos en orden alfábetico y filtrando imágenes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,49 @@
+#include <algorithm>
+#include <cstring>
 #include <iostream>
-#include "Lector.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 
-
-int main()
-{
-	Lector lec;
-
-	lec.contrat("foto.pdf",1);
-	lec.contrat("imagen.txt",2);
-	return 0;
+bool test_directory(char* directorio){
+	DIR* work_bench = opendir(directorio);
+	if(!work_bench){
+		return false;
+	} else{
+		closedir(work_bench);
+		return true;
+	}
 }
+
+int filter_function(const struct dirent *dir){
+	if(((strstr(dir->d_name, ".png") || strstr(dir->d_name, ".jpg")) || ((strstr(dir->d_name, ".jpeg")) || (strstr(dir->d_name, ".gif")))) || ((strstr(dir->d_name, ".JPEG")))){
+		return 1;
+	} else{
+		return 0;
+	}
+}
+
+int main(int argc, char* argv[]){
+	char* directorio = argv[1];
+	if(!test_directory(directorio)){
+		std:: cout << "failed to open directory\n";
+		return 1;
+	}
+	struct dirent **namelist;
+	int n,i;
+	n = scandir(directorio, &namelist, filter_function, versionsort);
+	if (n < 0)
+        perror("scandir");
+    else
+    {
+        for(i =0 ; i < n; ++i)
+        {
+            printf("%ld %s\n",namelist[i]->d_ino, namelist[i]->d_name);
+            free(namelist[i]);
+        }
+        free(namelist);
+    }
+    return 0;
+}
+
+///home/radamantis/Pictures
