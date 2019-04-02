5684cd6cbe2758dbc9def97ded067ce4ec31bfff
root <urenaesteban27@gmail.com>
2019-04-01 22:07:16 -0700

    Practicamente todo el codigo util

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <fstream>
+#include <vector>
 
 bool test_directory(char* directorio){
 	DIR* work_bench = opendir(directorio);
@@ -24,49 +26,54 @@ int filter_function(const struct dirent *dir){
 	}
 }
 
-int main(int argc, char* argv[]){
-	char* directorio = argv[1];
-	if(!test_directory(directorio)){
-		std:: cout << "failed to open directory\n";
+int lector(const char* directorio){
+	if(!test_directory(directorio)){ // Revisa si el posible abrir el directorio
+		std:: cout << "Fallo al abrir el directorio\n";
 		return 1;
 	}
-	struct dirent **namelist;
+	
+	struct dirent** imagenes; // Estructura que guarda la posicion de memoria y el nombre de las imagenes
 	int n,i;
-	n = scandir(directorio, &namelist, filter_function, versionsort);
+	n = scandir(directorio, &imagenes, filter_function, versionsort);
 	if (n < 0)
         perror("scandir");
     else
     {
         for(i =0 ; i < n; ++i)
         {
-            printf("%ld %s\n",namelist[i]->d_ino, namelist[i]->d_name);
-            free(namelist[i]);
+            printf("%ld %s\n",imagenes[i]->d_ino, imagenes[i]->d_name);
+            free(imagenes[i]);
         }
-        free(namelist);
+        free(imagenes);
     }
     
-    
-    
-    
-int cnt=0;
-Lector lec;
-for(int i =0; i<2; i++)
-{
-	
-if(cnt == 0)
-{	
-lec.contrat("prueba1.jpg",cnt);
-//string q se guarda aqui no sirve cuando llega al metodo
-}
-else if(cnt == 1)
-{
-lec.contrat("prueba2.jpg",cnt);
+    // Crear contratistas y seguir contratando hasta que no hayan mas imagenes por analizar
+	return 0;
 }
 
-    
-    
-    
+
+std::vector<char> leerBytes(char const* imagen)
+{
+	// Esto lee toda la imagen
+	ifstream ifs(imagen, ios::binary|ios::ate);
+	ifstream::pos_type pos = ifs.tellg();
+
+	std::vector<char> result(pos);
+
+	ifs.seekg(0, ios::beg);
+	ifs.read(&result[0], pos);
+
+	return result;
+}
+
+
+
+int main(int argc, char* argv[]){
+	// Se pasa el directorio con las imagenes como argumento del programa
+	if(argc != 2){ // Revisa si pasaron la carpeta como argumento del programa
+		return std::cerr << "Para utilizar el programa de manera correcta debe ingresar la carpeta con las imagenes como argumento del programa\n";
+	}
+	
+	lector(argv[1]);
     return 0;
 }
-
-///home/radamantis/Pictures
