9cd359c1af9342e124c1912e757d8d284071cfba
LuisValverde1296 <luis.valverdecruz@ucr.ac.cr>
2019-04-03 16:23:01 -0600

    Borrando documentos basura

--- Buzon/main2.cpp
+++
@@ -1,9 +0,0 @@
-#include "Buzon.h"
-#include <iostream>
-int main(){
-	Buzon* buzon = new Buzon(1, 0xB7304);
-	std::cout << "Esperando mensaje ... \n";
-	std::cout << buzon->miBuzon.mText;
-	buzon->recibir();
-	
-}
