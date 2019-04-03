97ea19dedfcf809321fb5e04a71d29c013b03d35
diegotrejos <48733767+diegotrejos@users.noreply.github.com>
2019-04-03 14:08:18 -0600

    Clase emisor,
    cada paquete trae, tag y luego datos
    el tag es un char
    el primer paquete debe traer tag y nombre del archivo

--- Emisor.h
+++ Emisor.h
@@ -2,24 +2,25 @@
 #include <unistd.h>
 #include <iostream>
 #include <fstream>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-
+#include <dirent.h>
+#include <fstream>
 #include <vector>
+#include <map>
+#include <stdlib.h> 
+#include <string.h>
+#include <iterator>
+
 using namespace std;
 class Emisor
 {
 public:
-	Emisor();
-	void recibe();//recibe datos del buzon
-	void Envio();
-	void archivar(vector<char>);//transforma datos a archivo
-	void ordene();//ordena los paquetes de informacion y los manda a archivar
-	void escribir(vector<char> result);
-	
-{
-
+	 Emisor(int msqid, std::vector<char> v);
+	void recibe(int msqid,vector<char>);//recibe datos del buzon
+	void envio();
+	void archivar();//transforma datos a archivo
+	void escribir(char tag, vector<char>);//escribe en archivo ya abierto
+	void creaArchivo(char tag, char* nombre);//abre archivo nuevo
 	~Emisor();
+	map<char,char*> archivos;//podria hacer mapas, tengo tag y nombre de archivo
 
 };
